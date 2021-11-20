47a21a6cab1241f271d22c96bf83b64bcda05a42
JuanGut <jdgutierrezc@uqvirtual.edu.co>
2021-11-20 16:15:56 -0500

    correccion pregunta

--- src/Componentes/CabeceraGestor.js
+++ src/Componentes/CabeceraGestor.js
@@ -6,42 +6,50 @@ import { LogoutButton } from "./Logout";
 import { Profile } from "./Profile";
 import '../Estilos/boton.css';
 import Pregunta from "./Pregunta";
-import {Link} from "react-router-dom";
 
 
-export default function CabeceraGestor(){
+export default function CabeceraGestor() {
 
-    const [visibilidad, setVisibilidad]= useState(false);
-        return (
-            <div>
-                <Navbar collapseOnSelect expand="lg" bg="success" variant="dark" wit>
-                    <Container>
+    const [visibilidad, setVisibilidad] = useState(false);
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="success" variant="dark" wit>
+                <Container>
 
-                        <img
-                            src={iconoU}
-                            width="70"
-                            height="70"
-                            className="rounded float-start"
+                    <img
+                        src={iconoU}
+                        width="70"
+                        height="70"
+                        className="rounded float-start"
 
-                        />
-                        <Navbar.Brand href="#home">Competencias 4.0</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                        <Navbar.Collapse id="responsive-navbar-nav">
-                            <Nav className="me-auto">
-                                <button class="btnPregunta" onClick={visibilidad ? <pregunta/>: null}>Preguntas</button>
-                                <button class="btnEncuesta">Encuesta</button>
-                                <button class="btnMaterial">Material</button>
-                                <Link>Home</Link>
-                            </Nav>
-                            <Profile />
-                            <Nav>
-                                <LogoutButton />
-                            </Nav>
-                        </Navbar.Collapse>
-                      
-                    </Container>
-                </Navbar>
-            </div>
-        );
+                    />
+                    <Navbar.Brand href="#home">Competencias 4.0</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <button class="btnPregunta" onClick={() => setVisibilidad(true)}>Preguntas</button>
+                            <button class="btnEncuesta">Encuesta</button>
+                            <button class="btnMaterial">Material</button>
+                        </Nav>
+                        <Profile />
+                        <Nav>
+                            <LogoutButton />
+                        </Nav>
+                    </Navbar.Collapse>
+
+                    {visibilidad ? (
+                        <>
+                            <Pregunta/>
+
+
+                        </>
+                    ) : (
+                        null
+                    )}
+
+                </Container>
+            </Navbar>
+        </div>
+    );
 
 }
