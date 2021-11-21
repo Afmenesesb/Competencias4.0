7a8db02f004eaa1c8a3d4b7efcfb3f8d5374c6ff
afmenesesb <afmenesesb@uqvirtual.edu.co>
2021-11-21 14:23:49 -0500

    Validación dominio

--- src/App.js
+++ src/App.js
@@ -8,26 +8,98 @@ import { Profile } from './Componentes/Profile';
 import { useAuth0 } from '@auth0/auth0-react';
 import CabeceraHome from './Componentes/CabeceraHome';
 import CabeceraGestor from './Componentes/CabeceraGestor';
+import Button from 'react-bootstrap/Button';
 
-
+function verificarDominio(dominio) {
+  if (dominio.includes('uqvirtual')) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 function App() {
-  const { isAuthenticated, isLoading } = useAuth0();
+  const { isAuthenticated, isLoading, user } = useAuth0();
+
+
   if (isLoading) return <h1>Loading...</h1>
 
-  return (
-    <div className="App">
-      {isAuthenticated ? (
-        <>
-          <CabeceraGestor/>
-          
+  if (isAuthenticated) {
+    if (user.email.includes('uniquindio')) {
+      return (
+        <div className="App">
+          {isAuthenticated ? (
 
-        </>
-      ) : (
-      <Cabecera/>
-      )}
-      
-    </div>
-  );
+            <CabeceraHome />
+
+          ) : (
+
+            <Cabecera />
+
+          )}
+
+        </div>
+
+      );
+
+    }
+    else {
+      if (user.email.includes('uqvirtual')) {
+        return (
+          <div className="App">
+            {isAuthenticated ? (
+
+              <CabeceraGestor />
+
+            ) : (
+
+              <Cabecera />
+
+            )}
+
+          </div>
+
+        );
+
+      }
+      else {
+        return (
+          <div className="App">
+          
+          <h1>NO TIENE ACCESO A LA PAGINA WEB</h1>
+          <button class="btn btn-danger">SALIR</button>
+    
+          </div>
+          
+    
+        );
+      }
+
+    }
+
+  }
+  else {
+    return (
+      <div className="App">
+        {isAuthenticated ? (
+
+          <Cabecera />
+
+        ) : (
+
+          <Cabecera />
+
+        )}
+
+      </div>
+
+    );
+
+
+
+  }
+
+
 }
 
 export default App;
