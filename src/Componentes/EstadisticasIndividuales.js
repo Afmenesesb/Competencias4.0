5f090a8796f40c3a59fa486f77d22600cf2624e5
JuanGut <jdgutierrezc@uqvirtual.edu.co>
2022-07-20 10:29:14 -0500

    cambio vista

--- src/Componentes/EstadisticasIndividuales.js
+++ src/Componentes/EstadisticasIndividuales.js
@@ -1,4 +1,4 @@
-import { setDoc, doc, collection} from "firebase/firestore";
+import { setDoc, doc, collection } from "firebase/firestore";
 import React, { Component, useState } from "react";
 import '../Estilos/gestorPregunta.css';
 import db from "../firebaseConfig";
@@ -10,16 +10,22 @@ export default function EstadisticasIndividuales() {
 
     return (
         <div id="formularioBusquedaEst" >
-            <form class="form1" onSubmit={handleSumit}>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Correo del estidiante</label>
-                    <input id="correo" value={values.id} onChange={handleInputChange} type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="id" />
+
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
                 </div>
-                <button id="btnGuardar" type="submit" class="btn btn-success" >Buscar</button>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
 
         </div>
