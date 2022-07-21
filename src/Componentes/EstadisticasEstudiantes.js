70e1b36683ce7657090ba7107867484131d5132b
afmenesesb <afmenesesb@uqvirtual.edu.co>
2022-07-21 16:00:15 -0500

    Grafica v1

--- src/Componentes/EstadisticasEstudiantes.js
+++ src/Componentes/EstadisticasEstudiantes.js
@@ -3,35 +3,20 @@ import React, { Component, useState } from "react";
 import db from "../firebaseConfig";
 import swal from 'sweetalert';
 import '../Estilos/estIndividual.css';
+import DiagramaRadar from "./Radar";
 
+function estadisticaActi(e) {
 
+}
+function estadistica(e) {
+
+}
 export default function EstadisticasEstudiantes() {
-    const data = {
-        labels: [
-          'Red',
-          'Blue',
-          'Yellow'
-        ],
-        datasets: [{
-          label: 'My First Dataset',
-          data: [300, 50, 100],
-          backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
-          ],
-          hoverOffset: 4
-        }]
-      };
-      const config = {
-        type: 'pie',
-        data: data,
-      };
-    
     return (
         <div id="estadisticasEstudiantes" class="mencuesta">
-            <div>Hola mundo</div>
             
+            <div>Hola mundo</div>
+            <DiagramaRadar/>
         </div>
 
 
