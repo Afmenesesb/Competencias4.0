49957ed7f4d3c1d7f1a344fe73f5f5d3edc65d8f
afmenesesb <afmenesesb@uqvirtual.edu.co>
2022-07-28 18:32:19 -0500

    Gestor v1

--- src/Componentes/Cuerpo.js
+++ src/Componentes/Cuerpo.js
@@ -170,7 +170,7 @@ export default function Cuerpo() {
     return (
         <div id="cuerpoPrin" class="cuerpo">
             <h1 id="contLor" class="parrafo1">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                <input type="texto">Escriba su texto acá</input>
             </h1>
             <div class="contenedorImg">
                 <img id="imgCont" class="imgCuerpo" src={infCont} ></img>
