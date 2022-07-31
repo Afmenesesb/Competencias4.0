964dd01196e0ca50e8042172a4c71dfaf9b5ecf0
JuanGut <jdgutierrezc@uqvirtual.edu.co>
2022-07-31 13:40:14 -0500

    Organizacion de graficas

--- src/Componentes/EstadisticasEstudiantes.js
+++ src/Componentes/EstadisticasEstudiantes.js
@@ -32,23 +32,23 @@ ChartJS.register(
   BarElement
 );
 var InformacionContable = 0;
-var Gestion = 0;
+var Gestion = 2;
 var AnalisisEconomico = 0;
-var Analisisyevolucion = 0;
+var Analisisyevolucion = 3;
 var Diseñoyprogramacion = 0;
-var InteligenciaEmocional = 0;
+var InteligenciaEmocional = 1;
 var Innovacion = 0;
 var Liderazgoeinfluencia = 0;
-var PensamientoAnalitico = 0;
+var PensamientoAnalitico = 5;
 var PensamientoCritico = 0;
 var Solucion = 0;
-var ConocimientoDigital = 0;
+var ConocimientoDigital = 3;
 var AprendizajeContinuo = 0;
-var ComunicacionDigital = 0;
+var ComunicacionDigital = 4;
 var Gestiondelainformacion = 0;
 var Liderazgoenred = 0;
 var Trabajoenred = 0;
-var VisionEstrategica = 0;
+var VisionEstrategica = 3;
 var Orientacionalcliente = 0;
 var competenciasEstudiante = ["Informacion Contable", "Gestion de organizaciones", "Analisis Economico",
   "Analisis y evolucion de sistemas", "Diseño y programacion de nuevas tecnologias", "Inteligencia Emocional",
@@ -158,14 +158,14 @@ const obtenerNotas = async () => {
       text: "El error fue: " + error
     })
   }
-  var speedCanvas= document.getElementById("speedChart");
+  var speedCanvas= document.getElementById("modConocimiento");
   var speedData={
 
     label:'#Respuestas correctas',
     data: [InformacionContable, Gestion, AnalisisEconomico],
   };
 
-  /*var chartOptions= {
+  var chartOptions= {
     Legend: {
 
       display: true,
@@ -175,7 +175,7 @@ const obtenerNotas = async () => {
         fontColor: 'black'
       }
     }
-  };*/
+  };
 
   var barChart= new ChartJS(speedCanvas,
     {
@@ -188,6 +188,78 @@ const obtenerNotas = async () => {
 
       }
     });
+    var speedCanvas2= document.getElementById("modCompetecias");
+    var speedData2={
+  
+      label:'#Respuestas correctas',
+      data: [ConocimientoDigital, AprendizajeContinuo, ComunicacionDigital, VisionEstrategica, Gestiondelainformacion, Liderazgoenred, Orientacionalcliente, Trabajoenred],
+    };
+  
+    /*var chartOptions= {
+      Legend: {
+  
+        display: true,
+        position: 'top',
+        labels: {
+          boxWidth:80,
+          fontColor: 'black'
+        }
+      }
+    };*/
+  
+    var barChart2= new ChartJS(speedCanvas2,
+      {
+        type: 'bar',
+        data: {
+          labels: ["Conocimiento digital",
+          'Aprendizaje continuo',
+          'Comunicacion Digital',
+          'Vision estrategica',
+          'Gestion de la informacion',
+          'Liderazgo en red',
+          'Orientacion al cliente',
+          'Trabajo en red'],
+          datasets: [speedData2]
+  
+        }
+      });
+
+    var speedCanvas3= document.getElementById("modActitud");
+    var speedData3={
+  
+      label:'#Respuestas correctas',
+      data: [Analisisyevolucion, Diseñoyprogramacion, Innovacion, InteligenciaEmocional, Liderazgoeinfluencia, PensamientoAnalitico, PensamientoCritico, Solucion],
+    };
+  
+    /*var chartOptions= {
+      Legend: {
+  
+        display: true,
+        position: 'top',
+        labels: {
+          boxWidth:80,
+          fontColor: 'black'
+        }
+      }
+    };*/
+  
+    var barChart3= new ChartJS(speedCanvas3,
+      {
+        type: 'bar',
+        data: {
+          labels: ['Analisis y evolucion de sistemas',
+          'Diseño y programacion de nuevas tecnologias',
+          'Innovacion,originalidad e iniciativa',
+          'Inteligencia emocional',
+          'Liderazgo e influencia social',
+          'Pensamiento analítico',
+          'Pensamiento crítico',
+          'Solucion de problemas'],
+          datasets: [speedData3]
+  
+        }
+      });
+      estadisticas();
 }
 
 
@@ -245,15 +317,15 @@ const estadisticas = () => {
     }
   }
 
-  var radarCanvas = document.getElementById("RadarChart");
+  var radarCanvas = document.getElementById("RadarChartCom");
   var radarData = {
     label: 'Preguntas Correctas',
     data: [ConocimientoDigital, AprendizajeContinuo, ComunicacionDigital, VisionEstrategica, Gestiondelainformacion, Liderazgoenred, Orientacionalcliente, Trabajoenred]
   };
 
-  var barChart = new ChartJS(radarCanvas, {
+  var radChart = new ChartJS(radarCanvas, {
 
-    type: 'bar',
+    type: 'radar',
     data: {
       labels: ["Conocimiento digital",
         'Aprendizaje continuo',
@@ -264,6 +336,43 @@ const estadisticas = () => {
         'Orientacion al cliente',
         'Trabajo en red'],
       datasets: [radarData]
+    }
+  });
+  var radarCanvas2 = document.getElementById("RadarChartCon");
+  var radarData2 = {
+    label: 'Preguntas Correctas',
+    data: [InformacionContable, Gestion, AnalisisEconomico]
+  };
+
+  var radChart2 = new ChartJS(radarCanvas2, {
+
+    type: 'radar',
+    data: {
+      labels: ['Información contable',
+      'Gestión de organizaciones',
+      'Analisis Economico'],
+      datasets: [radarData2]
+    }
+  });
+  var radarCanvas3 = document.getElementById("RadarChartAct");
+  var radarData3 = {
+    label: 'Preguntas Correctas',
+    data: [Analisisyevolucion, Diseñoyprogramacion, Innovacion, InteligenciaEmocional, Liderazgoeinfluencia, PensamientoAnalitico, PensamientoCritico, Solucion]
+  };
+
+  var radChart3 = new ChartJS(radarCanvas3, {
+
+    type: 'radar',
+    data: {
+      labels: ['Analisis y evolucion de sistemas',
+      'Diseño y programacion de nuevas tecnologias',
+      'Innovacion,originalidad e iniciativa',
+      'Inteligencia emocional',
+      'Liderazgo e influencia social',
+      'Pensamiento analítico',
+      'Pensamiento crítico',
+      'Solucion de problemas'],
+      datasets: [radarData3]
     }
   });
 }
@@ -483,15 +592,14 @@ export default function EstadisticasEstudiantes() {
         <button onClick={(e) => { mostrarEstadisticaM(e) }} id="btEM" type="button" class="btn btn-success">Estadisticas MODULO</button>
       </div>
       <div id="graficasEstudiantes" class="mencuesta">
-        <canvas id="RadarChart" width="600" height="400"></canvas>
-        <div id="grfCon" class="mengraf">CONOCIMIENTO<Radar data={dataConocimiento} /></div>
-        <div id="grfAct" class="mengraf">ACTITUD<Radar data={dataActitud} /></div>
+        <canvas id="RadarChartCom" width="600" height="400">Competencias</canvas>
+        <canvas id="RadarChartCon" width="600" height="400">Conocimiento</canvas>
+        <canvas id="RadarChartAct" width="600" height="400">Actitud</canvas>
       </div>
       <div id="graficasEstudiantesM" class="mencuesta">
-        <canvas id="speedChart" width="600" height="400"></canvas>
-        <div id="grfConM" class="mengraf">CONOCIMIENTO por MODULO<Bar data={dataModulosCono} /></div>
-        <div id="grfComM" class="mengraf">COMPETENCIAS por MODULO<Bar data={dataModulosComp} /></div>
-        <div id="grfActM" class="mengraf">ACTITUD por MODULO<Bar data={dataModulosAct} /></div>
+        <canvas id="modConocimiento" width="600" height="400">Conocimiento por Modulo</canvas>
+        <canvas id="modCompetecias" width="600" height="400">Competencias por Modulo</canvas>
+        <canvas id="modActitud" width="600" height="400">Actitud por Modulo</canvas>
       </div>
     </div>
 
